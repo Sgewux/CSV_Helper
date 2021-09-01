d8d76caf8539f91c416da459672810892344e4b5
Sebastian Andrade <sebasandroide05@gmail.com>
2021-09-01 17:14:53 -0500

    Working on the is_correct_type function and fixing some bugs

--- main.py
+++ main.py
@@ -3,6 +3,44 @@ import csv
 import sys
 
 
+def is_correct_type(item, expected_type):
+    """Function for check if a given item has the excpected type.
+       returns True if the expected type matches the item, False if not
+
+    Parameters:
+        -item: the piece of data we are going to check.
+        -expected_type: the expected data type for item.
+    """
+    if item != None:
+        if expected_type in {int, float, (int, float)}:
+            try:
+                if (float(item)*10%10) == 0: 
+                    
+                    if expected_type in {int, (int, float)}:
+                        return True
+                    elif expected_type == float:
+                        return False
+                
+                elif expected_type in {float, (int, float)}:
+                    return True
+
+            except ValueError: #This means that the value was not an integer or float
+                return False
+
+        elif expected_type == bool and item.strip().upper() in {'TRUE', 'FALSE'}:
+            return True
+
+        elif expected_type == str and item.strip().upper() not in {'TRUE', 'FALSE'}:
+            try:
+                float(item)
+                return False
+            
+            except ValueError: #This means that we could not convert the item to float, wich means is not a numeric value
+                return True
+    else:
+        return False
+                
+
 def find_type_errors(col_vs_type, reader_obj, file_path):
     """Function for find value errors in a given csv file
 
@@ -14,19 +52,12 @@ def find_type_errors(col_vs_type, reader_obj, file_path):
 
     for row in reader_obj:
         for col, type_ in col_vs_type:
-            if not type(type_) == set: #if we are dealing with a single expected value not more than one
-                if not type(row[col]) == type_:
-                    print('wrong')
-            else : #if we are actually dealing with more than one expected value
-                if not ( type(row[col]) in type_ or row[col] in type_ ):
-                    print(type(row[col]))
-                    print('WROng')
-
-
-
-
-    
-
+            #try:
+            if not is_correct_type(row[col], type_):
+            
+                print('no')
+            #except Exception as e:
+                #print(e)
 
 
 def main(file_paths):
@@ -58,13 +89,13 @@ def main(file_paths):
                     row_types.append(float)
 
                 elif column_value_type == 'N':
-                    row_types.append({int, float})
+                    row_types.append((int, float))
 
                 elif column_value_type == 'B':
                     row_types.append(bool)
 
             name_type_pairs = tuple(zip(column_names, row_types))
-            find_type_errors(name_type_pairs, dict_reader, '.')
+            find_type_errors(name_type_pairs, dict_reader, p)
 
 
 
